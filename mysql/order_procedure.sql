116504294eac779def50a3e82114adfeaa1340a2
ftconan <1508907793@qq.com>
2019-10-21 16:34:27 +0800

    add cursor

--- mysql/order_procedure.sql
+++ mysql/order_procedure.sql
@@ -43,12 +43,63 @@ END //
 
 DELIMITER ;
 
-SHOW CREATE PROCEDURE ordertotal;
+# SHOW CREATE PROCEDURE ordertotal;
+#
+# SHOW PROCEDURE STATUS LIKE 'ordertotal';
+#
+# CALL ordertotal(20005, 0, @total);
+# SELECT @total;
+#
+# CALL ordertotal(20005, 1, @total);
+# SELECT @total;
 
-SHOW PROCEDURE STATUS LIKE 'ordertotal';
 
-CALL ordertotal(20005, 0, @total);
-SELECT @total;
+DROP PROCEDURE IF EXISTS processorders;
 
-CALL ordertotal(20005, 1, @total);
-SELECT @total;
+DELIMITER //
+
+CREATE PROCEDURE processorders()
+
+BEGIN
+    -- Declare local variables
+    DECLARE done BOOLEAN DEFAULT 0;
+    DECLARE o INT;
+    DECLARE t DECIMAL(8,2);
+
+    -- Declare the cursor
+    DECLARE ordernumbers CURSOR
+    FOR
+    SELECT order_num FROM orders;
+    -- Declare continue handler
+    DECLARE CONTINUE HANDLER FOR SQLSTATE '02000' SET done=1;
+
+    -- Create a table to store the results
+    CREATE TABLE IF NOT EXISTS ordertotals
+        (order_num INT, total DECIMAL(8,2));
+
+    -- Open the cursor
+    OPEN ordernumbers;
+
+    -- Loop through all rows
+    REPEAT
+        -- Get order number
+        FETCH ordernumbers INTO o;
+
+        -- Get the total for this order
+        CALL ordertotal(o, 1, t);
+
+        -- Insert order and total into ordertotals
+        INSERT INTO ordertotals(order_num, total)
+        VALUES (o, t);
+
+        -- End of loop;
+        UNTIL done END REPEAT;
+
+        -- Close the cursor;
+        CLOSE ordernumbers;
+END //
+
+DELIMITER ;
+
+SELECT *
+FROM ordertotals;
